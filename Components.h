e97d4cdd7513256ab108728bba54c4d67081599b
Craig <craigag@protonmail.com>
2021-01-18 21:45:16 +0000

    Added a SerializeableComponent base class for components to inherit from. This will ensure the components have the serialize and deserialize methods.

--- Components.h
+++ Components.h
@@ -1,6 +1,15 @@
 #pragma once
 
-struct Position
+#include <fstream>;
+
+struct SerializeableComponent
+{
+	virtual ~SerializeableComponent() {};
+	virtual void Serialize(std::ofstream & file) = 0;
+	virtual int Deserialize(const char* buffer, int offset) = 0;
+};
+
+struct Position : SerializeableComponent
 {
 	Position()
 	{
@@ -12,36 +21,68 @@ struct Position
 	virtual ~Position() 
 	{
 	};
+	void Serialize(std::ofstream& file) {
+
+	}
+	int Deserialize(const char* buffer, int offset) {
+		return offset;
+	}
 
 	float x{ 0.0f }, y{ 0.0f }, z{ 0.0f };
-
-	
 };
 
 struct MeshRenderer
 {
-	
+	void Serialize(std::ofstream& file) {
+
+	}
+	int Deserialize(const char* buffer, int offset) {
+		return offset;
+	}
 	virtual ~MeshRenderer() {};
 };
 
 struct AI
 {
+	void Serialize(std::ofstream& file) {
 
+	}
+	int Deserialize(const char* buffer, int offset) {
+		return offset;
+	}
 	virtual ~AI() {};
 };
 
 struct RigidBody
 {
+	void Serialize(std::ofstream& file) {
+
+	}
+	int Deserialize(const char* buffer, int offset) {
+		return offset;
+	}
 	virtual ~RigidBody() {};
 };
 
 struct Sprite
 {
+	void Serialize(std::ofstream& file) {
+
+	}
+	int Deserialize(const char* buffer, int offset) {
+		return offset;
+	}
 	virtual ~Sprite() {};
 };
 
 struct Model
 {
+	void Serialize(std::ofstream& file) {
+
+	}
+	int Deserialize(const char* buffer, int offset) {
+		return offset;
+	}
 	virtual ~Model() {};
 };
 
